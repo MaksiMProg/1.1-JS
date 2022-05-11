274fbfb25bd0db21e081cda1745a5f526dd82600
MaksiMProg <101975865+MaksiMProg@users.noreply.github.com>
2022-05-11 15:56:11 +0300

    Add files via upload

---
+++ 1.1-JS/js/script.js
@@ -0,0 +1,43 @@
+alert('Привет, это работа Макса Мухи'); // 1-ое задание
+
+let planetName = 'earth'; // 2-ое задание
+let age = 16;
+let brokenCar = 'Volkswagen';
+let headerColor = 'white';
+let admin = confirm('Ты администратор?');
+console.log(planetName, age, brokenCar, headerColor);
+
+let myName = 'Maks '; // 3-ье задание
+let mySurName = 'Myxa';
+console.log('Привет меня зовут: ' + myName + mySurName, 'мне ' + age + 'лет');
+
+let checkerName = prompt('Какое твоё имя?'); // 4-ое задание
+console.log('Мою работу проверял: ' + checkerName);
+
+let str = "String"; // 5-ое задание
+let num = 2000;
+let testSkiped = false;
+let month = undefined;
+let data = null;
+console.log(typeof str);
+console.log(typeof num);
+console.log(typeof testSkiped);
+console.log(typeof month);
+console.log(typeof data);
+
+let newStr = "37"; // 6-ое задание 
+console.log(Boolean(newStr));
+console.log(Number(newStr));
+
+let newValue = true;
+console.log(String(newValue));
+console.log(Number(newValue));
+
+let newNum = 2007;
+console.log(String(newNum));
+console.log(Boolean(newNum));
+
+let name = 'Ivan'; // 7-ое задание
+let surname;
+surname = name;
+console.log(surname);
